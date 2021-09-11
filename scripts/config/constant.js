f9de6282bef373b1ff9eb1e6c72dee7870f867be
Lambort <857309716@qq.com>
2021-09-12 01:27:36 +0800

    fix (dev server) : fix find port increase

--- scripts/config/constant.js
+++ scripts/config/constant.js
@@ -1,41 +1,12 @@
 const path = require('path');
+const portFinder = require('portfinder-sync');
 
-const net = require('net');
-
-const portUsed = (port) => {
-    const server = net.createServer().listen(port);
-    let isOccupied;
-    server.on('listening', () => {
-        server.close();
-        isOccupied = false;
-    });
-    server.on('error', (err) => {
-        if (err.code == 'EADDRINUSE') {
-            isOccupied = true;
-        }
-    });
-    return isOccupied ? new Error('port occupied') : port;
-};
-
-const tryUsePort = (port) => {
-    const PORT_LIMIT = Math.pow(256, 2);
-    let availablePort;
-    for (let i = port; i < PORT_LIMIT; i++) {
-        let response = portUsed(port);
-        if (response instanceof Error) {
-            continue;
-        } else {
-            availablePort = i;
-            break;
-        }
-    }
-    return port;
-};
+const BASE_PORT = 3000;
 
 const PROJECT_PATH = path.resolve(__dirname, '../../');
 
 const SERVER_HOST = '0.0.0.0';
-const SREVER_PORT = tryUsePort(3000);
+const SREVER_PORT = portFinder.getPort(BASE_PORT);
 
 module.exports = {
     PROJECT_PATH,
